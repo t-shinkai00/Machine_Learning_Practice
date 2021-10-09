87711c2a9ea7a5ef4b6c30a850e91fc627b42c1d
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-09 22:47:20 +0900

    fix: typo

--- supervised/multiple-regression-analysis.py
+++ supervised/multiple-regression-analysis.py
@@ -21,7 +21,7 @@ x=df.drop(labels=["Target"], axis=1).values
 
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, t_train, t_test=train_test_split(x, t, test_size=0.3, random_sate=0)
+x_train, x_test, t_train, t_test=train_test_split(x, t, test_size=0.3, random_state=0)
 
 from sklearn.linear_model import LinearRegression
 
