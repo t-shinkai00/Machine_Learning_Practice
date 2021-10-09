493fdceb954201d8793ede3f64097205b821af46
t-shinkai00 <t.shinkai00@gmail.com>
2021-10-09 22:51:04 +0900

    add comments

--- supervised/multiple-regression-analysis.py
+++ supervised/multiple-regression-analysis.py
@@ -12,20 +12,25 @@ x, t, columns=dataset.data, dataset.target, dataset.feature_names
 # print(type(x),x.shape)
 # print(type(t),t.shape)
 
+# データフレーム作成
 df=pd.DataFrame(x,columns=columns)
 df["Target"]=t
 # print(df)
 
+# 入力と出力の切り分け
 t=df["Target"].values
 x=df.drop(labels=["Target"], axis=1).values
 
+# 学習用データとテストデータ切り分け
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, t_train, t_test=train_test_split(x, t, test_size=0.3, random_state=0)
 
+# 学習モデルのインポート
 from sklearn.linear_model import LinearRegression
 
 model=LinearRegression()
 # print(model)
 
+# 学習
 model.fit(x_train, t_train)
